ff44960b5929c3c430815947266d8337c2dbc9b0
Jeremy Newson <jknewson@usgs.gov>
2018-01-10 12:23:32 -0700

    added Ilink interface to handle links

--- src/app/shared/interfaces/IParameter.interface.ts
+++ src/app/shared/interfaces/IParameter.interface.ts
@@ -4,13 +4,13 @@
 // copyright:   2017 WiM - USGS
 // authors:  Tonia Roddick - USGS Wisconsin Internet Mapping
 // purpose: interface for the settings/config.json configuration.resources[i].methods.uriList[i].parameters[i]
+import { ILink } from "./ILink.interface";
 
 export interface Iparameter {
     name: string;
     description: string;
     type: string;
     optional?: boolean;
-    link?: string;
-    linkName?: string;    
+    link?: ILink;  
     value: string;    
 }
